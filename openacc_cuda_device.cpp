d4a5dae3f2cec74e461f62a32bc9eab69fbb994a
George Bisbas <g.bisbas18@imperial.ac.uk>
2020-12-21 14:55:59 +0000

    Kens example working HPC SDK

--- openacc_cuda_device.cpp
+++ openacc_cuda_device.cpp
@@ -9,45 +9,52 @@
 extern "C" float saxpy_dev(float, float, float);
 
 #pragma acc routine seq
-extern "C" float foo(float, float);
-
-inline
-__device__ __half __hmul(const half a, const half b);
+extern "C" __half saxpy_dev_half(__half, __half, __half);
 
 int main(int argc, char **argv)
 {
-  float *x, *y, tmp;
-  float a = 0.16f;
-  float b = 0.16f;
-  float c = 0.16f; 
-  fprintf(stdout, "c = %f\n", c);
-  
-  c = foo(a, b);
-  fprintf(stdout, "c = %f\n", c);
-  
-  int n = 1<<20, i;
+  float *x, *y; 
+  __half *xf, *yf;
+ 
+  int n = 1<<23, i;
+  fprintf(stdout, "[n] = %d\n", n);
 
   x = (float*)malloc(n*sizeof(float));
   y = (float*)malloc(n*sizeof(float));
+  xf = (__half*)malloc(n*sizeof(__half));
+  yf = (__half*)malloc(n*sizeof(__half));
 
-  #pragma acc data create(x[0:n]) copyout(y[0:n])
+  #pragma acc data create(x[0:n]) copyout(y[0:n]) create(xf[0:n]) copyout(yf[0:n])
   {
-    #pragma acc kernels
+#pragma acc parallel loop default(present)
+    for( i = 0; i < n; i++)
     {
-      for( i = 0; i < n; i++)
-      {
-        x[i] = 1.0f;
-        y[i] = 0.0f;
-      }
+      x[i] = 1.0f;
+      y[i] = 0.0f;
+    }
+
+#pragma acc parallel loop default(present)
+    for( i = 0; i < n; i++)
+    {
+      xf[i] = __half(1.0f);
+      yf[i] = __half(0.0f);
     }
       
-#pragma acc parallel loop
+#pragma acc parallel loop default(present)
     for( i = 0; i < n; i++ )
     {
       y[i] = saxpy_dev(2.0, x[i], y[i]);
     }
+
+#pragma acc parallel loop default(present)
+    for( i = 0; i < n; i++ )
+    {
+      yf[i] = saxpy_dev_half(__float2half(2.0f), xf[i], yf[i]);
+    }
   }
 
   fprintf(stdout, "y[0] = %f\n",y[0]);
+  fprintf(stdout, "yf[0] = %f\n",__half2float(yf[0]));
+
   return 0;
 }
